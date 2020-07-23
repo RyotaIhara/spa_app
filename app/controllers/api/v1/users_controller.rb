112b9c6abd9235876b944cf6eb193680ee0cae1e
program <ihara0531@gmail.com>
2020-07-24 03:46:48 +0900

    users index complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  # ActiveRecordのレコードが見当たらなければ404
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { error: '404 not found' }, status: 404
+  end
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    render json: @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <table>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>ユーザー名</th>
+          <th>年齢</th>
+          <th>性別</th>
+          <th>備考</th>
+        </tr>
+        <tr v-for="u in users" :key="u.id">
+          <td>{{ u.id }}</td>
+          <td>{{ u.name }}</td>
+          <td>{{ u.age }}</td>
+          <td v-if="u.gender == true">男</td>
+          <td v-else>女</td>
+          <td>{{ u.note }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
-    }
-  }
-}
+      users: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("/api/v1/users.json")
+      .then((response) => (this.users = response.data));
+  },
+};
 </script>
 
 <style scoped>
