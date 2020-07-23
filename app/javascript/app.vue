41652c0b83259130a3aa3e5dc471dc452ed18abb
program <ihara0531@gmail.com>
2020-07-24 04:10:40 +0900

    user index bootstrap apply

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,24 +1,26 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-        <tr>
-          <th>ID</th>
-          <th>ユーザー名</th>
-          <th>年齢</th>
-          <th>性別</th>
-          <th>備考</th>
-        </tr>
-        <tr v-for="u in users" :key="u.id">
-          <td>{{ u.id }}</td>
-          <td>{{ u.name }}</td>
-          <td>{{ u.age }}</td>
-          <td v-if="u.gender == true">男</td>
-          <td v-else>女</td>
-          <td>{{ u.note }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="container">
+    <div id="app">
+      <table class="table">
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>ユーザー名</th>
+            <th>年齢</th>
+            <th>性別</th>
+            <th>備考</th>
+          </tr>
+          <tr v-for="u in users" :key="u.id">
+            <td>{{ u.id }}</td>
+            <td>{{ u.name }}</td>
+            <td>{{ u.age }}</td>
+            <td v-if="u.gender == true">男</td>
+            <td v-else>女</td>
+            <td>{{ u.note }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
--- app/javascript/packs/homes_vue.js
+++ app/javascript/packs/homes_vue.js
@@ -9,10 +9,9 @@ Vue.use(BootstrapVue)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#hello',
-    data: {
-      message: "Can you say hello?"
-    },
-    components: { App }
-  })
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
 })
