bc4be1dd1b52e6ec2b078b09d8e1d301f142d7cf
program <ihara0531@gmail.com>
2020-07-24 03:07:18 +0900

    user controllr,model,table　作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  :name => 'ユーザー１', 
+  :age => '10', 
+  :gender => true,
+  :note => '備考１'
+)
+User.create(
+  :name => 'ユーザー２', 
+  :age => '20', 
+  :gender => false,
+  :note => '備考２'
+)
+User.create(
+  :name => 'ユーザー３', 
+  :age => '30', 
+  :gender => true,
+  :note => '備考３'
+)
