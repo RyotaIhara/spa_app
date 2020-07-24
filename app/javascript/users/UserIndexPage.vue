8003393f44c51476e1833c920320aaaa555e81a4
program <ihara0531@gmail.com>
2020-07-24 20:12:48 +0900

    complete link

--- app/javascript/users/UserIndexPage.vue
+++ app/javascript/users/UserIndexPage.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <div>
+      <router-link :to="{ name: 'UserNewPage' }">新規作成</router-link>
+    </div>
     <table class="table">
       <tbody>
         <tr>
-          <th>ID</th>
           <th>ユーザー名</th>
           <th>年齢</th>
           <th>性別</th>
           <th>備考</th>
         </tr>
         <tr v-for="u in users" :key="u.id">
-          <td>{{ u.id }}</td>
-          <td>{{ u.name }}</td>
+          <td>
+            <router-link :to="{ name: 'UserEditPage', params: { id: u.id } }">{{ u.name }}</router-link>
+          </td>
           <td>{{ u.age }}</td>
           <td>{{ u.gender }}</td>
           <td>{{ u.note }}</td>
