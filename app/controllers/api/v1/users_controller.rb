2cb6a4095136a6aef15846ab25283f46e31ebbe0
program <ihara0531@gmail.com>
2020-07-24 21:29:10 +0900

    complete delete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show,:update]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   # 拾えなかったExceptionが発生したら500 Internal server errorを応答する
   rescue_from Exception, with: :render_status_500
@@ -35,6 +35,11 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user.destroy!
+    head :no_content
   end
 
   private
--- app/javascript/users/UserIndexPage.vue
+++ app/javascript/users/UserIndexPage.vue
@@ -10,6 +10,7 @@
           <th>年齢</th>
           <th>性別</th>
           <th>備考</th>
+          <th>操作</th>
         </tr>
         <tr v-for="u in users" :key="u.id">
           <td>
@@ -18,25 +19,63 @@
           <td>{{ u.age }}</td>
           <td>{{ u.gender }}</td>
           <td>{{ u.note }}</td>
+          <td>
+            <button @click="deleteTarget = u.id; showModal = true">Delete</button>
+          </td>
         </tr>
       </tbody>
     </table>
+    <modal v-if="showModal" @cancel="showModal = false" @ok="deleteUser(); showModal = false;">
+      <div slot="body">Are you sure?</div>
+    </modal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 
+import Modal from "shared/Modal.vue";
+
 export default {
+  components: {
+    Modal,
+  },
   data: function () {
     return {
       users: [],
+      showModal: false,
+      deleteTarget: -1,
+      errors: "",
     };
   },
   mounted() {
-    axios
-      .get("/api/v1/users.json")
-      .then((response) => (this.users = response.data));
+    this.updateUsers();
+  },
+  methods: {
+    deleteUser: function () {
+      if (this.deleteTarget <= 0) {
+        console.warn("deleteTarget should be grater than zero.");
+        return;
+      }
+
+      axios
+        .delete(`/api/v1/users/${this.deleteTarget}`)
+        .then((response) => {
+          this.deleteTarget = -1;
+          this.updateUsers();
+        })
+        .catch((error) => {
+          console.error(error);
+          if (error.response.data && error.response.data.errors) {
+            this.errors = error.response.data.errors;
+          }
+        });
+    },
+    updateUsers: function () {
+      axios
+        .get("/api/v1/users.json")
+        .then((response) => (this.users = response.data));
+    },
   },
 };
 </script>
